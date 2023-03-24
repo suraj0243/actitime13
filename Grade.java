993dd454a20ec0db05f041581d1c90dbd7f9d1dd
suraj0243 <128788803+suraj0243@users.noreply.github.com>
2023-03-24 13:39:26 +0530

    Add files via upload

---
+++ Grade.java
@@ -0,0 +1,38 @@
+class  Grades
+{
+ public static void main(String[]args)
+ {
+  double result=100;
+  if (result<35 && result>=0)
+   {
+    system.out.println("Fail. Better Luck next time...!");
+  }
+   else if(result=35)
+  {
+   system.out.println("Just Pass");
+  }
+   else if(result>35 && result<=50)
+  {
+   system.out.println("second class");
+}
+  else if(result>50 && result <=70)
+{
+system.out.println("first class");
+}
+else if(result>70 && result<=90)
+{
+system.out.println("Distinction");
+}
+else if(result>90 && result<=100)
+{
+system.out.println("Extraordinary");
+}
+else
+{
+system.out.println("You are not from this planet");
+}
+}
+}
+
+
+
